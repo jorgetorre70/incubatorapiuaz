1862566621622f8433c84341348ca92a72082709
jorgetorre70 <jorgetorre1022@gmail.com>
2024-06-19 04:39:20 -0600

    update the .env file

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const { router } = require("./routes/indexroutes");
 const app = express();
+require("dotenv").config(); // Load environment variables from .env file
 
 app.use(cors());
-// app.use(express.json());
 app.use("/v1", router);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 function main() {
   app.listen(PORT, () => {
--- controllers/awsconnection.js
+++ controllers/awsconnection.js
@@ -1,13 +1,18 @@
 const awsIot = require("aws-iot-device-sdk");
-
+const private_key = process.env.PRIVATE_KEY;
+const certificado_dispo = process.env.CERTIFICADO_DISPO;
+const AmazonRootCA1 = process.env.AMAZON_ROOT_CA1;
 // Create a persistent IoT device connection
 const device = awsIot.device({
   clientId: "marco_esp32dht11",
   host: "a3todvehpsou32-ats.iot.us-east-2.amazonaws.com",
   port: 8883,
-  keyPath: "./private_key.key",
-  certPath: "./certificado_dispo.crt",
-  caPath: "./AmazonRootCA1.pem",
+  keyPath: private_key,
+  certPath: certificado_dispo,
+  caPath: AmazonRootCA1,
+  //keyPath: "./private_key.key",
+  // certPath: "./certificado_dispo.crt",
+  // caPath: "./AmazonRootCA1.pem",
 });
 
 device.on("connect", function () {
